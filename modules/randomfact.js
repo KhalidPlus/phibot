cc1cdabdf76f10f188278b24ef7f8eea4f7d1f96
Zipdox <42157341+Zipdox@users.noreply.github.com>
2020-07-13 16:34:59 +0200

    Update randomfact.js

--- modules/randomfact.js
+++ modules/randomfact.js
@@ -3,7 +3,7 @@ commands['fact'] = function(){
         headers: { 'User-Agent': 'request' }
     }).then(response => response.text()).then(data => {
         const searchPage = new JSDOM(data);
-        const fact = searchPage.window.document.getElementById('z').innerText.split('\n')[0];
+        const fact = searchPage.window.document.getElementById('z').innerHTML.split('<br>')[0];
         greenText('fact');
     });
 }
