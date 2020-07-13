29ddb4f39a3eccc381821b17bdbc73c284e33955
Zipdox <42157341+Zipdox@users.noreply.github.com>
2020-07-13 16:27:23 +0200

    user agent for random fact

--- modules/randomfact.js
+++ modules/randomfact.js
@@ -1,12 +1,7 @@
 commands['fact'] = function(){
-    var options = {
-        url: `http://randomfactgenerator.net/`,
-        headers: {
-        'User-Agent': 'request'
-        }
-    };
-    
-    fetch('http://randomfactgenerator.net/').then(response => response.text()).then(data => {
+    fetch('http://randomfactgenerator.net/', {
+        headers: { 'User-Agent': 'request' }
+    }).then(response => response.text()).then(data => {
         const searchPage = new JSDOM(data);
         const fact = searchPage.window.document.getElementById('z').innerText.split('\n')[0];
         greenText('fact');
